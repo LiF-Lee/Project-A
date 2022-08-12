c8ed50b0e606e666c8aae4ef5cf48fe86f7d5747
LiF <leesj.dev@gmail.com>
2022-08-13 01:59:18 +0900

    ENH: Player 스폰 Effect 추가

--- Assets/Scripts/Movement_Controller.cs
+++ Assets/Scripts/Movement_Controller.cs
@@ -10,6 +10,7 @@ public class Movement_Controller : MonoBehaviour
     private int _default_rotation = -45;
     private Vector3 forward, right;
     private GameObject UI_Canvas;
+    private bool Respawn_Period = true;
 
     private void Start()
     {
@@ -18,14 +19,20 @@ public class Movement_Controller : MonoBehaviour
         forward.y = 0;
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+        Invoke("Respawn_Period_End", 2f);
     }
 
     private void Update()
     {
-        if (Input.anyKey)
+        if (Input.anyKey && Respawn_Period == false)
         {
             Move();
         }
+    }
+
+    private void Respawn_Period_End()
+    {
+        Respawn_Period = false;
     }
 
     private void Move()
