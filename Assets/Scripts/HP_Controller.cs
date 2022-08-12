959e842110f5ae731297100e2b1e8faf2dc41e8d
LiF <leesj.dev@gmail.com>
2022-08-13 01:18:06 +0900

    ENH: player HP_Slider가 현재 체력에 따라 유동적으로 변화합니다

---
+++ Assets/Scripts/HP_Controller.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class HP_Controller : MonoBehaviour
+{
+    private Player_Controller HP;
+    private bool HP_Change_Detect = false;
+    private int Current_HP, Max_HP, Save_Current_HP, Save_Max_HP;
+    [SerializeField] private Slider HP_Slider;
+    [SerializeField] private TextMeshProUGUI HP_Text;
+
+    private void Start()
+    {
+        HP = gameObject.GetComponent<Player_Controller>();
+        Get_Lastest_HP();
+        Save_Current_HP = Current_HP;
+        Save_Max_HP = Max_HP;
+        Update_HP_Text();
+    }
+
+    private void Update()
+    {
+        Get_Lastest_HP();
+        Update_HP_Text();
+        if ((Save_Current_HP != Current_HP || Save_Max_HP != Max_HP) && HP_Change_Detect == false)
+        {
+            HP_Change_Detect = true;
+            Save_Current_HP = Current_HP;
+            StartCoroutine(Update_HP_Slider());
+        }
+    }
+
+    public void Accumulative_HP(int value)
+    {
+        HP._HP.Player_Current_HP += value;
+    }
+
+    private void Get_Lastest_HP()
+    {
+        Current_HP = HP._HP.Player_Current_HP;
+        Max_HP = HP._HP.Player_Max_HP;
+    }
+    
+    private void Update_HP_Text()
+    {
+        if (HP_Text != null)
+        {
+            HP_Text.text = $"<b>{Current_HP}</b><color=\"yellow\"> / </color><b>{Max_HP}</b>";
+        }
+    }
+
+    private bool is_HP_Over_Value()
+    {
+        return (Current_HP / (Max_HP * 0.01f) > 100 && HP_Slider.value == 1) || (Current_HP / (Max_HP * 0.01f) < 0 && HP_Slider.value == 0);
+    }
+
+    private IEnumerator Update_HP_Slider()
+    {
+        yield return null;
+        while (HP_Change_Detect)
+        {
+            yield return null;
+            HP_Slider.value = Mathf.MoveTowards(HP_Slider.value, Current_HP / (Max_HP * 0.01f) * 0.01f, Time.deltaTime * 0.6f);
+
+            if ((int)(HP_Slider.value * 100) == (int)(Current_HP / (Max_HP * 0.01f)) || is_HP_Over_Value())
+            {
+                HP_Change_Detect = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -3,9 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class HP
+{
+    public int Player_Current_HP;
+    public int Player_Max_HP;
+}
+
 public class Player_Controller : MonoBehaviour
 {
-    [SerializeField] private float _crash_area = -10f;
+    [Header("Player Setting")]
+    public HP _HP;
     
     private void Update()
     {
@@ -14,6 +22,7 @@ public class Player_Controller : MonoBehaviour
 
     private void Check_Player_Crash()
     {
+        float _crash_area = -10f;
         if (transform.position.y <= _crash_area)
         {
             Kill_Player();
